686d41740b3595b4a768d39cf937a2f9a5f9cf94
Sergi Carol <sergicarol35@gmail.com>
2018-05-09 11:57:24 +0200

    Added clustering method for large data

--- R_script.R
+++ R_script.R
@@ -99,9 +99,37 @@ nd = 3
 # Cluster
 
 Psi = pca$ind$coord[, 1:nd]
+# dist_matrix = dist(Psi)
+# write.matrix(dist_matrix, "dist_matrix.csv")
+#cluster <- hclust(dist_matrix, method='ward.D2')
 
-dist_matrix = dist(Psi)
+# Cluster for large data
+n1 = 100
+k1 <- kmeans(Psi, n1)
+k2 <- kmeans(Psi, n1)
+table(k2$cluster,k1$cluster)
+clas <- (k2$cluster-1)*n1+k1$cluster
+freq <- table(clas) 
+cdclas <- aggregate(as.data.frame(Psi),list(clas),mean)[,2:(nd+1)]
+cdclas
+d2 <- dist(cdclas)
+h2 <- hclust(d2,method="ward.D2",members=freq) # COMPARE THE COST
+plot(h2)
+barplot(h2$height[(nrow(cdclas)-40):(nrow(cdclas)-1)]) # PLOT OF THE LAST 39 AGGREGATIONS 
+nc = 7
+c2 <- cutree(h2,nc)
+cdg <- aggregate((diag(freq/sum(freq)) %*% as.matrix(cdclas)),list(c2),sum)[,2:(nd+1)] # WHY WEIGHT 
+k6 <- kmeans(Psi,centers=cdg)
 
-write.matrix(dist_matrix, "dist_matrix.csv")
-cluster <- hclust(dist_matrix, method='ward.D2')
+Bss <- sum(rowSums(k6$centers^2)*k6$size)
+Wss <- sum(k6$withinss)
+Ib6 <- 100*Bss/(Bss+Wss)
+Ib6
+
+plot(Psi, type="n",main="Clustering of countries in 2 clusters")
+text(Psi,col=k6$cluster, cex = 0.6)
+abline(h=0,v=0,col="gray")
+legend("bottomright",c("c1","c2", "c3", "c4", "c5", "c6", "c7"),pch=20,col=c(1:7))
+#points(cdg, col="blue")
+text(k6$centers,labels=c("G1","G2", "G3", "G4", "G5", "G6", "G7"),col="blue")
 
